babc5c3f3ce50119c4b9c7e09f6c115b90d067b1
hadinajafi <hadinajafi.77@gmail.com>
2020-10-27 13:19:49 +0330

    added Lob annotation to note text, added getters/setters

--- src/main/java/guru/springframework/recipedemo/models/Notes.java
+++ src/main/java/guru/springframework/recipedemo/models/Notes.java
@@ -1,9 +1,6 @@
 package guru.springframework.recipedemo.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
 public class Notes {
@@ -14,5 +11,31 @@ public class Notes {
 
     @OneToOne
     private Recipe recipe;
+
+    @Lob
     private String recipeNote;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNote() {
+        return recipeNote;
+    }
+
+    public void setRecipeNote(String recipeNote) {
+        this.recipeNote = recipeNote;
+    }
 }
