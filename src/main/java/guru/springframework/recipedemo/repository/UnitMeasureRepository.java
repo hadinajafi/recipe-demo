11570f9dd7c9f5387870ec8e044158532a6d6458
hadinajafi <hadinajafi.77@gmail.com>
2020-10-30 13:53:17 +0330

    added UnitMeasure crud repository

--- src/main/java/guru/springframework/recipedemo/repository/UnitMeasureRepository.java
+++ src/main/java/guru/springframework/recipedemo/repository/UnitMeasureRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.recipedemo.repository;
 import guru.springframework.recipedemo.models.UnitMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitMeasureRepository extends CrudRepository<UnitMeasure, Long> {
+    Optional<UnitMeasure> findByDescription(String description);
 }
