57c77f3b83847f7b95e23eca12ff3485d659234e
hadinajafi <hadinajafi.77@gmail.com>
2020-10-30 13:54:48 +0330

    get recipes from recipe repository

--- src/main/java/guru/springframework/recipedemo/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipedemo/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package guru.springframework.recipedemo.controllers;
 
+import guru.springframework.recipedemo.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController{
 
+    RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"", "/", "index"})
     public String getIndex(Model model){
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
