bdb35aee214aad7e6e385186ff55e7b9afd9281f
hadinajafi <hadinajafi.77@gmail.com>
2020-10-30 13:55:51 +0330

    added Hashset init to the Set properties

--- src/main/java/guru/springframework/recipedemo/models/Recipe.java
+++ src/main/java/guru/springframework/recipedemo/models/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.recipedemo.models;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -15,6 +16,8 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String direction;
 
     @Lob
@@ -27,12 +30,12 @@ public class Recipe {
     private Notes note;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @ManyToMany
     @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns =
     @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
