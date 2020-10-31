c97f7f1f4270184adc528c1381a36dccf38c61ee
hadinajafi <hadinajafi.77@gmail.com>
2020-10-31 11:31:07 +0330

    used slf4j logger

--- src/main/java/guru/springframework/recipedemo/services/RecipeServiceImp.java
+++ src/main/java/guru/springframework/recipedemo/services/RecipeServiceImp.java
@@ -2,11 +2,13 @@ package guru.springframework.recipedemo.services;
 
 import guru.springframework.recipedemo.models.Recipe;
 import guru.springframework.recipedemo.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImp implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -17,7 +19,7 @@ public class RecipeServiceImp implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
-
+        log.debug("entered recipe service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
