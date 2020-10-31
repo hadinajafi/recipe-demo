0327f219077db5206b82e9053ddde95bf200f28b
hadinajafi <hadinajafi.77@gmail.com>
2020-10-31 11:30:57 +0330

    used slf4j logger

--- src/main/java/guru/springframework/recipedemo/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipedemo/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package guru.springframework.recipedemo.controllers;
 
 import guru.springframework.recipedemo.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController{
 
@@ -16,6 +18,7 @@ public class IndexController{
 
     @RequestMapping({"", "/", "index"})
     public String getIndex(Model model){
+        log.debug("getting index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
